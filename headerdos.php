b4bb63e6568cf7902cfa8ea9f496bfd4d7fa9957
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-11 15:11:37 -0600

    Se cambiaron ciertos archivos

--- headerdos.php
+++ headerdos.php
@@ -1,31 +1,28 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-colorarriba">
-      <div class="container-fluid">
-        <img class="logoprinc" src="img/logo.png" alt="icon"> <a class="navbar-brand" href="#">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="menu navbar-nav">
-                <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="index.php">Principal</a>
-            </li>
-                    <li class="nav-item <?php if ($currentPage === 'Citas en linea.php') echo 'active'; ?>">
-                        <a class="nav-link" aria-current="page" href="Citas en linea.php">Citas en linea</a>
-                    </li>
-                    <li class="nav-item <?php if ($currentPage === 'Mapa.php') echo 'active'; ?>">
-                        <a class="nav-link" aria-current="page"  href="Mapa.php">Mapa</a>
-                    </li>
-                    <li class="nav-item <?php if ($currentPage === 'registros.php') echo 'active'; ?>">
-                        <a class="nav-link" href="registros.php">Registrarse</a>
-                </li>
-                </ul>
-            </div>
+  <nav class="navbar navbar-expand-lg bg-colorarriba">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img class="logoprinc" src="img/logo.png" alt="icon">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="index.php">Principal</a>
+          </li>
+          <li class="nav-item <?php if ($currentPage === 'Citas en linea.php') echo 'active'; ?>">
+            <a class="nav-link" aria-current="page" href="Citas en linea.php">Citas en linea</a>
+          </li>
+          <li class="nav-item <?php if ($currentPage === 'Mapa.php') echo 'active'; ?>">
+            <a class="nav-link" aria-current="page" href="Mapa.php">Mapa</a>
+          </li>
+          <li class="nav-item <?php if ($currentPage === 'registros.php') echo 'active'; ?>">
+            <a class="nav-link" href="registros.php">Registrarse</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <div>
-
-    
-  </header>
+    </div>
+  </nav>
+</header>
